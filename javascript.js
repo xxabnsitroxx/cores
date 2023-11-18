4b5a91c3a8e070e8bc1d2a86ccc28070dee730d9
xxabnsitroxx <56044585+xxabnsitroxx@users.noreply.github.com>
2023-11-17 21:04:45 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,77 @@
+//bloco 1
+function bt1bloco1cor1() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'red';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'red';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'red';
+}
+
+
+function bt1bloco1cor2() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'yellow';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'yellow';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'yellow';
+}
+
+function bt1bloco1cor3() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'blue';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'blue';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'blue';
+}
+
+function bt1bloco1cor4() {
+    document.getElementById('secao1lin1col1').style.backgroundColor = 'green';
+    document.getElementById('secao2lin1col1').style.backgroundColor = 'green';
+    document.getElementById('secao3lin1col1').style.backgroundColor = 'green';
+}
+
+//bloco 2
+function bt1bloco2cor1() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'red';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'red';
+}
+
+function bt1bloco2cor2() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'yellow';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'yellow';
+}
+
+function bt1bloco2cor3() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'blue';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'blue';
+}
+
+function bt1bloco2cor4() {
+    document.getElementById('secao2lin1col2').style.backgroundColor = 'green';
+    document.getElementById('secao3lin1col2').style.backgroundColor = 'green';
+}
+
+//bloco 3
+function bt1bloco3cor1() {
+    document.getElementById('secao3lin2col3').style.backgroundColor = 'red';
+}
+function bt1bloco3cor2() {
+    document.getElementById('secao3lin2col3').style.backgroundColor = 'yellow';
+}
+function bt1bloco3cor3() {
+    document.getElementById('secao3lin2col3').style.backgroundColor = 'blue';
+}
+function bt1bloco3cor4() {
+    document.getElementById('secao3lin2col3').style.backgroundColor = 'green';
+}
+
+//bloco 4
+function bt1bloco4cor1() {
+    document.getElementById('secao3lin2col4').style.backgroundColor = 'red';
+}
+function bt1bloco4cor2() {
+    document.getElementById('secao3lin2col4').style.backgroundColor = 'yellow';
+}
+function bt1bloco4cor3() {
+    document.getElementById('secao3lin2col4').style.backgroundColor = 'blue';
+}
+function bt1bloco4cor4() {
+    document.getElementById('secao3lin2col4').style.backgroundColor = 'green';
+}
+
+
+
